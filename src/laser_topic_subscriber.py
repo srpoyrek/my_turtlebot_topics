dd62ff1817757e6a611a1ba78f8bac7bbb2081ac
srpoyrek <shreyaspoyrekar@gmail.com>
2021-02-01 06:02:02 +0000

    Updated laser_topic_subscriber to return the 3 data side datas

--- src/laser_topic_subscriber.py
+++ src/laser_topic_subscriber.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan
+import time
 
 class LaserSubClass(object):
     def __init__(self):
@@ -8,6 +9,9 @@ class LaserSubClass(object):
         self._laser_sub=rospy.Subscriber('/kobuki/laser/scan',LaserScan,self.class_callback)
         #subscriber message type object
         self._laser_msg_object = LaserScan()
+        self.front = 0.0
+        self.right = 0.0
+        self.left = 0.0
 
     #callback function for the subscriber
     def class_callback(self,msg):
@@ -19,7 +23,19 @@ class LaserSubClass(object):
     #returns the laser data
     def get_laserdata(self):
         return self._laser_msg_object
-
+    
+    #gets data left right and front laser readings
+    def crash_data(self):
+        self.front = self._laser_msg_object.ranges[360]
+        self.right = self._laser_msg_object.ranges[0]
+        self.left = self._laser_msg_object.ranges[719]
+        rospy.loginfo("Distance in front = "+str(self.front))
+        rospy.loginfo("Distance in right = "+str(self.left))
+        rospy.loginfo("Distance in left = "+str(self.right))
+    
+    #returns crash data as dict
+    def get_crash_data(self):
+        return {"front": self.front, "left":self.left, "right":self.right}
 
 if __name__ == "__main__":
     rospy.init_node('laser_sub_node',log_level=rospy.INFO)
@@ -34,10 +50,9 @@ if __name__ == "__main__":
         ctrl_c = True
     
     rospy.on_shutdown(shutdownhook)
-
+    time.sleep(2)
     while not ctrl_c:
         data = laser_sub_object.get_laserdata()
-        rospy.loginfo(data)
+        laser_sub_object.crash_data()
+        #rospy.loginfo(data.ranges)
         rate.sleep()
-
-    
